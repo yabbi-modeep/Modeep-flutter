116703828f4ea84886a75829b576d3f8e70f0d04
seoyerin <us3544974@gmail.com>
2024-09-13 13:53:49 +0900

    DEL :: 불필요한 코드 제거

--- lib/presentation/main/view/main_screen.dart
+++ lib/presentation/main/view/main_screen.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:modeep/core/modeep_color.dart';
-import 'package:modeep/presentation/main/widget/main_screen_app_bar_widget.dart';
-import 'package:modeep/presentation/main/widget/main_screen_bottom_service_widget.dart';
-import 'package:modeep/presentation/main/widget/main_screen_service_title_widget.dart';
-import 'package:modeep/presentation/main/widget/main_screen_service_widget.dart';
-import 'package:modeep/presentation/main/widget/main_screen_slider_widget.dart';
-import 'package:modeep/presentation/main/widget/main_screen_title_widget.dart';
+
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
@@ -18,19 +13,6 @@ class MainScreen extends StatelessWidget {
         padding: const EdgeInsets.only(left: 25, right: 25),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            MainScreenAppBarWidget(),
-            const SizedBox(height: 18),
-            MainScreenTitleWidget(),
-            const SizedBox(height: 19),
-            MainScreenSliderWidget(),
-            const SizedBox(height: 18),
-            MainScreenServiceTitleWidget(),
-            const SizedBox(height: 24),
-            MainScreenServiceWidget(),
-            const SizedBox(height: 40),
-            MainScreenBottomServiceWidget()
-          ],
         ),
       ),
     );
